cd786ef4797e6df25c8ada89f60b981fc5012e58
momose tomohiro <momose-tomohiro@gb-sense.com>
2020-06-22 19:05:26 +0900

    lsコマンドの実装、-lオプションの一部実装

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,6 +9,10 @@ import (
 )
 
 func main() {
+
+	var lFlag = flag.Bool("l", false, "File Attribute")
+	flag.Parse()
+
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
@@ -16,6 +21,23 @@ func main() {
 	fileInfos, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if *lFlag {
+		fmt.Println("total", len(fileInfos))
+		for _, fileInfo := range fileInfos {
+
+			fmt.Printf("%v", fileInfo.Mode())
+
+			fmt.Printf(" %v", fileInfo.Size())
+
+			fmt.Printf(" %v", os.Getgid())
+
+			fmt.Printf(" %v ", fileInfo.ModTime().Format("Jan 2 15:04"))
+
+			fmt.Println(fileInfo.Name())
+		}
+		return
 	}
 
 	for _, fileInfo := range fileInfos {
